ef0fe10dee1c46e61282b24c7b1184b03f02fa49
kevin.li <fly911338@gmail.com>
2022-02-10 10:47:08 +0800

    Add embedded-redis dependency for test to fix RedisServiceTest without redis throw exception

--- src/test/java/com/kevin/linebotdemo/service/RedisServiceTest.java
+++ src/test/java/com/kevin/linebotdemo/service/RedisServiceTest.java
@@ -1,15 +1,15 @@
 package com.kevin.linebotdemo.service;
 
 import lombok.val;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import redis.embedded.RedisServer;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -17,6 +17,23 @@ class RedisServiceTest {
 
     @Autowired
     private RedisService underTest;
+
+    private static RedisServer redisServer;
+
+
+    @BeforeAll
+    static void startRedis() {
+        redisServer = RedisServer.builder()
+                .port(6379)
+                .setting("maxmemory 128M") //maxheap 128M
+                .build();
+        redisServer.start();
+    }
+
+    @AfterAll
+    static void stopRedis() {
+        redisServer.stop();
+    }
 
     @Test
     void set() {
